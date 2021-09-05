28afde5aff46c4a03c8055fd951230df3b1a305d
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-09-05 19:00:23 +0300

    adds the function akaname

---
+++ index.js
@@ -0,0 +1,29 @@
+function akaname(e,day,month,year) {
+    e.preventDefault();
+    day = document.getElementById("day").value;
+    month=document.getElementById("month").value;
+    year=document.getElementById("year").value;
+    console.log(year);
+
+}
+
+// function akaname(e){
+//     e.preventDefault();
+
+//     var day = document.getElementById("day").value;
+//     var month = document.getElementById("month").value;
+//     var year = document.getElementById("year").value;
+
+
+//     alert("i was born "+ day + "the year" + year)
+//     // console.log( day, month, year);
+
+    
+
+// }
+
+// function resetform(){
+//     day="";
+//     month="";
+//     year="";
+// };
