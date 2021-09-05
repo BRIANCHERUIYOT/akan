f35a5bf8ac09fd1e8fca2da6a96106daa4900404
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-09-05 21:52:52 +0300

    retrieved my values from the forms and validated the form

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-function akaname(e,day,month,year) {
-    e.preventDefault();
-    day = document.getElementById("day").value;
-    month=document.getElementById("month").value;
-    year=document.getElementById("year").value;
-    console.log(year);
 
-}
+function akaname(){
 
-// function akaname(e){
-//     e.preventDefault();
+    var maleName =["kwasi (sunday)", "kwadwo (Monday)","kwabena (Tuesday)","kwaku (Wednesday)","yaw (Thursday)","kofi (Friday)","kwame (Saturday)"];
+    var femaleName=["Akosua (sunday)", "Adwao (Monday)","Abenaa (Tuesday)", "Akua (Wednesday)","Yaa (Thursday)","Afua (Friday)","Ama (Saturday)"];
 
-//     var day = document.getElementById("day").value;
-//     var month = document.getElementById("month").value;
-//     var year = document.getElementById("year").value;
+    // e.preventDefault();
+    var day = parseInt(document.getElementById("day").value)
+    var month = parseInt(document.getElementById("month").value)
+    var year = document.getElementById("year").value;
+    var Male = document.getElementById("male").value;
+    var Female = document.getElementById("female").value;
+    var Rname = document.getElementById("aka-name").value;
 
 
-//     alert("i was born "+ day + "the year" + year)
-//     // console.log( day, month, year);
+    if (day== null || day == "", month == null || month== "", year == null || year == "") {
+        alert("Please Fill All Required Field");
+        return false;
+      }
+
+      if( day <= 0 || day>31)
+      {
+          alert("enter valid date"); 
+          return false;
+      }
+       else if(month <=0 || month > 12)
+       {
+           alert("enter valid month");
+           return false;
+       }
+    var yearA = year.slice(0,2);
+    alert(yearA);
+
+    // let sum =  day + 12;
+    // alert(sum);
 
     
 
-// }
+}
 
 // function resetform(){
 //     day="";
 //     month="";
 //     year="";
 // };
+
+// if((day < 1) || (day>31) || (isNaN(day))) {
+    //     alert("Please select a valid date");
+    // 
