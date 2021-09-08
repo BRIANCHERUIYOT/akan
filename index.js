deafc0f5221baa29075b1ee7d9a478df1771b1d0
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-09-08 13:22:33 +0300

    adds a event to code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function akaname(e) {
+function akaname(event) {
 
     var maleName = ["kwasi", "kwadwo", "kwabena", "kwaku", "yaw", "kofi", "kwame"];
     var femaleName = ["Akosua", "Adwao", "Abenaa", "Akua ", "Yaa", "Afua", "Ama"];
     var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursady", "Friday", "Saturday"];
-    e.preventDefault();
+    event.preventDefault();
 
 
 
